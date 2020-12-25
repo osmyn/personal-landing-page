126f40762380674d37220e6c92aa8be94e59d907
Rusty Divine <rusty.osmyn@gmail.com>
2020-12-25 08:37:19 -0600

    log msg

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -159,6 +159,7 @@
       timeout: 40000
     }).done( function(msg){
       // if (msg.trim() == 'ok') {
+        console.log(msg);
         this_form.find('.loading').slideUp();
         this_form.find('.sent-message').slideDown();
         this_form.find("input:not(input[type=submit]), textarea").val('');
